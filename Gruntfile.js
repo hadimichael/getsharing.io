9fde8963e1328b33f7293680211158b3853387f5
Hadi Michael <hadimichael@gmail.com>
2014-04-24 13:37:05 +1000

    temporarily comment out modernizr

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,7 +411,7 @@ module.exports = function (grunt) {
         'cssmin',
         'uglify',
         'copy:dist',
-        'modernizr',
+        //'modernizr',
         'rev',
         'usemin',
         'htmlmin'
